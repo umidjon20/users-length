2ade621ff34b8e5f23187c4593b0bb20b188e121
Umid Amirkhulov <uamirqulov5@gmail.com>
2023-01-23 05:36:05 -0700

    users length

---
+++ js/script.js
@@ -0,0 +1,81 @@
+let users = [
+   {
+      name: 'Kamol'
+     },
+   {
+      name: 'Dilshod'
+   },
+   {
+      name: 'Umar'
+   },
+   {
+      name: 'Jahongir'
+   },
+   {
+      name: 'Muhammadali'
+   },
+   {
+      name: 'Jasur'
+   },
+   {
+      name: 'Jamshid'
+   },
+   {
+      name: 'Maftuna'
+   },
+   {
+      name: 'Umid'
+   },
+   {
+      name: 'Tura'
+   }
+]
+
+for(let i = 0;i < users.length;i++){
+
+      if(users[i].name.length > 5) {
+         console.log(users[i].name + 'bu  ism 4 harfdan kup')
+      }else {
+         console.log(users[i].name + 'bu ismda 4 harf bor')
+    }
+   }
+
+
+
+
+
+
+
+
+
+
+   
+// function calcLength(usersData){
+// let result = users[i].name.length
+// for(let i = 0;i < users.length;i++){
+//    if(result > 5) {
+//       console.log(users[i].name + 'bu  ism 4 harfdan kup')
+//    }else {
+//       console.log(users[i].name + 'bu ismda 4 harf bor')
+//    }
+// }
+// }
+
+
+//  let result = users[i].name.length
+//  console.log(result)
+ 
+// if(result > 5) {
+//    console.log (users[i].name + ' ' + 'bu  ism 4 harfdan kup')
+//  }else {
+//     console.log(users[i].name + ' ' + 'bu ismda 4 harf bor')
+//  }
+
+
+
+
+
+// for(let i = 0; i > 10; i++){
+//    calcLength(users[i])
+//    // console.log(users)
+// }
