cb069ad986bf2337456975d32eb45c32d932d73f
Umid Amirkhulov <uamirqulov5@gmail.com>
2023-01-23 05:53:30 -0700

    some changes

--- js/script.js
+++ js/script.js
@@ -34,9 +34,9 @@ function calclength(){
 for(let i = 0;i < users.length;i++){
 
       if(users[i].name.length > 5) {
-         console.log(users[i].name + 'bu  ism 4 harfdan kup')
+         console.log(users[i].name + ' '+ 'bu  ism 4 harfdan kup')
       }else {
-         console.log(users[i].name + 'bu ismda 4 harf bor')
+         console.log(users[i].name + ' ' + 'bu ismda 4 harf bor')
     }
    }
 }
@@ -51,32 +51,3 @@ calclength()
 
 
 
-// function calcLength(usersData){
-// let result = users[i].name.length
-// for(let i = 0;i < users.length;i++){
-//    if(result > 5) {
-//       console.log(users[i].name + 'bu  ism 4 harfdan kup')
-//    }else {
-//       console.log(users[i].name + 'bu ismda 4 harf bor')
-//    }
-// }
-// }
-
-
-//  let result = users[i].name.length
-//  console.log(result)
- 
-// if(result > 5) {
-//    console.log (users[i].name + ' ' + 'bu  ism 4 harfdan kup')
-//  }else {
-//     console.log(users[i].name + ' ' + 'bu ismda 4 harf bor')
-//  }
-
-
-
-
-
-// for(let i = 0; i > 10; i++){
-//    calcLength(users[i])
-//    // console.log(users)
-// }
